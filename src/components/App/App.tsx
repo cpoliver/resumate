d1a2d9e298f64f91a1b42e10065599eb7fc22274
Charles Oliver <charles.oliver@iamcloud.com>
2021-09-17 07:44:02 +0100

    add chakra-ui

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
+import { Stack } from "@chakra-ui/react";
 import React from "react";
 
 import { AppBar } from "components";
 
 export const App = () => (
-  <div>
+  <Stack bg="tomato" bottom={0} left={0} pos="absolute" right={0} top={0}>
     <AppBar />
-  </div>
+  </Stack>
 );
