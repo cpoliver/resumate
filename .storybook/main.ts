46b737781df6e9581deecbdf7d359d9164a7755f
Charles Oliver <charles.oliver@iamcloud.com>
2021-09-26 00:10:24 +0100

    fix storybook/chakra config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  webpackFinal: async (config) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        "@emotion/core": "@emotion/react",
+        "emotion-theming": "@emotion/react",
+      },
+    },
+  }),
 }
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,11 @@
 import React from "react"
-import { Stack } from "@chakra-ui/react"
-
 
 import { ThemeProvider } from "../src/components"
 
 export const decorators = [
   (Story) => (
     <ThemeProvider>
-      <Stack bottom={0} flex={1} left={0} pos="relative" right={0} spacing={0} top={0} >
         <Story />
-      </Stack>
     </ThemeProvider>
   )
 ]
