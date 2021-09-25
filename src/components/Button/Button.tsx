009cff8b71e397c0468fc484a0791091ba280931
Charles Oliver <charles.oliver@iamcloud.com>
2021-09-25 12:54:20 +0100

    add plain IconButton variant

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,37 +6,37 @@ import * as Icon from "../Icon/Icon"
 export type ButtonProps = Omit<IconButtonProps, "aria-label">
 
 export const ActivityFeedButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Activity feed" icon={<Icon.HeartOutlineIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Activity feed" icon={<Icon.HeartOutlineIcon />} />
 )
 
 export const CommentButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Comment" icon={<Icon.CommentIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Comment" icon={<Icon.CommentIcon />} />
 )
 
 export const DirectMessagesButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Direct messages" icon={<Icon.DirectMessagesIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Direct messages" icon={<Icon.DirectMessagesIcon />} />
 )
 
 export const EllipsisButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="MoreOptions" icon={<Icon.EllipsisIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="MoreOptions" icon={<Icon.EllipsisIcon />} />
 )
 
 export const EmojiButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Emoji" icon={<Icon.EmojiIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Emoji" icon={<Icon.EmojiIcon />} />
 )
 
 export const FindPeopleButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Find people" icon={<Icon.FindPeopleIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Find people" icon={<Icon.FindPeopleIcon />} />
 )
 
 export const HomeButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Home" icon={<Icon.HomeIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Home" icon={<Icon.HomeIcon />} />
 )
 
 export const NewPostButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="New post" icon={<Icon.NewPostIcon />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="New post" icon={<Icon.NewPostIcon />} />
 )
 
 export const ProfileButton: React.FC<ButtonProps> = (buttonProps) => (
-  <IconButton {...buttonProps} aria-label="Activity feed" icon={<Avatar name="C O" size="xs" />} />
+  <IconButton variant="plain" {...buttonProps} aria-label="Activity feed" icon={<Avatar name="C O" size="xs" />} />
 )
--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -2,7 +2,18 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import React from "react"
 
 const theme = extendTheme({
-  // TODO: add any theme overrides
+  components: {
+    Button: {
+      variants: {
+        plain: {
+          bg: "none",
+          _hover: {
+            bg: "none",
+          },
+        },
+      },
+    },
+  },
 })
 
 export const ThemeProvider: React.FC = ({ children }) => <ChakraProvider theme={theme}>{children}</ChakraProvider>
