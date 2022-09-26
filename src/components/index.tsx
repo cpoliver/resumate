816120b12c6b0547a76eac84e5efd150dba9e8ae
Charles P Oliver <charles@cocoa.consulting>
2022-09-26 09:47:54 +0300

    chore: add exports to components/index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,13 @@
 export * from "./ColorModeSwitcher/ColorModeSwitcher"
 
+export * from "./ContactDetails/ContactDetails"
+
+export * from "./Links/Links"
+
 export * from "./Name/Name"
 
+export * from "./Summary/Summary"
+
 export * from "./ThemeProvider/ThemeProvider"
+
+export * from "./WorkHistory/WorkHistory"
