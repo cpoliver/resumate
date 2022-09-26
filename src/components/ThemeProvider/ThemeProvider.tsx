62964db6e90fdde547b69d3d59f76d8ce7766f45
Charles P Oliver <charles@cocoa.consulting>
2022-09-26 09:11:36 +0300

    refactor: remove redundant DynamicColorMode component

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,14 +1,11 @@
 import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react"
 import React from "react"
 
-import { DynamicColorMode } from "./DynamicColorMode"
 import { theme } from "theme"
 
 export const ThemeProvider: React.FC = ({ children }) => (
   <>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <ChakraProvider theme={extendTheme(theme)}>
-      <DynamicColorMode>{children}</DynamicColorMode>
-    </ChakraProvider>
+    <ChakraProvider theme={extendTheme(theme)}>{children}</ChakraProvider>
   </>
 )
